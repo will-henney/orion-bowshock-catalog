1dd3d464fcf908d8d5f237b034befab9f20f2e13
William Henney <will@henney.org>
2020-08-19 23:25:58 -0500

    Experiment more with Gaussian Mixture Models

--- notebooks/First look at statistics of applying circle_fit to all sources.py
+++ notebooks/First look at statistics of applying circle_fit to all sources.py
@@ -412,22 +412,27 @@ ax.plot([0.0, 10.0], [0.0, 10.0], lw=0.5, alpha=0.5, color='k', zorder=-1)
 m = np.isfinite(dff_merge["Pi"]) & np.isfinite(dff_merge["Lambda"])
 sn.kdeplot(dff_merge[m]["Pi"], dff_merge[m]["Lambda"], ax=ax, 
            bw=(0.05, 0.025), levels=[0.5, 1.0, 2.0, 4.0, 8.0, 12.0], norm=PowerNorm(0.5),
-           cmap="Purples",
+           cmap="Purples", zorder=1,
           )
 
 
 
 ax.errorbar(x=dff_merge["Pi"], y=dff_merge["Lambda"], xerr=dff_merge["e_Pi"], yerr=dff_merge["d_Lambda"], 
-            fmt='none', alpha=0.3)
+            fmt='none', alpha=0.3, zorder=2)
+
+sizes = np.empty_like(dff_merge["Pi"])
+m = dff_merge["Arc"] == "out"
+sizes[m] = 200.0
+sizes[~m] = 100.0
 
 for group, color in groups_and_colors:
     m = dff_merge["Group"] == group
     ax.scatter(x=dff_merge[m]["Pi"], y=dff_merge[m]["Lambda"], 
-               marker="o", c=[color], label=group, edgecolors="w")
+               marker="o", c=[color], s=sizes[m], label=group, edgecolors="w", zorder=3)
 ax.legend(ncol=2).set_title("Group")    
 ax.set(xlim=[0.35, 15.0], ylim=[0.35, 15.0], xscale="log", yscale="log")
 ax.set_aspect("equal")
-ax.set(xlabel=r"$\Pi_\mathrm{in}$", ylabel=r"$\Lambda_\mathrm{in}$")
+ax.set(xlabel=r"$\Pi$", ylabel=r"$\Lambda$")
 fig.savefig(ROOT_PATH / "all-sources-merge-Pi-Lambda.pdf")
 # -
 
@@ -436,5 +441,138 @@ fig.savefig(ROOT_PATH / "all-sources-merge-Pi-Lambda.pdf")
 # It appears that the shape distribution is bimodal (at least).  There is a cluster at $(\Pi, \Lambda) = (2, 2)$ and another at $(\Pi, \Lambda) \approx (3, 3)$.  Then some outliers around $(1.4, 1.4)$ and other outliers with $\Pi > 5$. 
 #
 # *We should model this with a Gaussian Mixture Model when we have more data.*  (See Jake VdP book) 
+
+# ## Fit Gaussian Cluster Model to shape data
+
+from sklearn.mixture import GaussianMixture as GMM
+
+# Put data in form expected by scikit-learn (and without NaNs)
+
+m = np.isfinite(dff_merge["Pi"]) & np.isfinite(dff_merge["Lambda"])
+X = np.array(list(zip(np.log10(dff_merge[m]["Pi"]), np.log10(dff_merge[m]["Lambda"]))))
+
+gmm = GMM(n_components=4).fit(X)
+
+labels = gmm.predict(X)
+
+labels
+
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.scatter(10**X[:, 0], 10**X[:, 1], c=labels, s=40, cmap='rainbow')
+ax.set(
+    xscale="log", yscale="log", 
+    xlim=[0.75, 15], ylim=[0.75, 15],
+    xlabel=r"Planitude, $\Pi$", ylabel=r"Alatude, $\Lambda$",
+)
+ax.set_aspect("equal");
+
+# So, using 4 components, it prefers to not separate the (1.4, 1.4) clump, instead fitting one component to the spur, which is probably spurious. It also fits a component to the high-$\Pi$ tail, which is more justified.
+
+# ### Bayesian Gaussian Mixture to automatically decide how many components
+
+from sklearn.mixture import BayesianGaussianMixture
+
+bgmm = BayesianGaussianMixture(n_components=4, verbose=1).fit(X)
+
+bgmm.weights_
+
+blabels = bgmm.predict(X)
+
+blabels
+
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.scatter(10**X[:, 0], 10**X[:, 1], c=blabels, s=40, cmap='rainbow')
+ax.set(
+    xscale="log", yscale="log", 
+    xlim=[0.75, 15], ylim=[0.75, 15],
+    xlabel=r"Planitude, $\Pi$", ylabel=r"Alatude, $\Lambda$",
+)
+ax.set_aspect("equal");
+
+# So, using the default parameters means that it decides that two components are enough.  One (purple) gets the main ridge with \(\Lambda \approx \Pi\), while the other gets the outliers.
+
+bgmm = BayesianGaussianMixture(n_components=30, verbose=1, weight_concentration_prior=1000.0).fit(X)
+
+bgmm.weights_
+
+blabels = bgmm.predict(X)
+blabels
+
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.scatter(10**X[:, 0], 10**X[:, 1], c=blabels, s=40, cmap='rainbow')
+ax.set(
+    xscale="log", yscale="log", 
+    xlim=[0.75, 15], ylim=[0.75, 15],
+    xlabel=r"Planitude, $\Pi$", ylabel=r"Alatude, $\Lambda$",
+)
+ax.set_aspect("equal");
+
+# However much we fiddle with the parameters to try and favor multiple components, it only wants to fit one component to the main diagonal.  Although it can be persuaded to split the outliers up into different components.
+
+# ### Use pomegranate library to incorporate per-sample weights
+#
+# With scikit.learn, we can't have different weights for each source (sample in ML parlance).  But we can do that with the pomegranate library. 
+
+from pomegranate import GeneralMixtureModel, MultivariateGaussianDistribution
+
+rel_err = np.hypot(dff_merge[m]["e_Pi"]/dff_merge[m]["Pi"], dff_merge[m]["e_Lambda"]/dff_merge[m]["Lambda"])
+weights = 1.0/rel_err**2
+weights[rel_err == 0.0] = np.nanmedian(weights)
+max_weight = 4.0*np.nanmedian(weights)
+weights[weights > max_weight] = max_weight
+weights[::10]
+
+N_COMPONENTS = 4
+
+model = GeneralMixtureModel.from_samples(
+    distributions=MultivariateGaussianDistribution,
+    n_components=N_COMPONENTS,
+    X=X,
+    weights=weights,
+)
+
+plabels = model.predict(X)
+plabels
+
+xmin, xmax = 0.75, 15
+ymin, ymax = 0.75, 15
+NP = 1000
+xx, yy = np.meshgrid(
+    np.linspace(xmin, xmax, NP),
+    np.linspace(ymin, ymax, NP),
+)
+x_ = np.array(list(zip(xx.flatten(), yy.flatten())))
+p_components = model.predict_proba(np.log10(x_)).reshape((NP, NP, N_COMPONENTS))
+p_tot = model.probability(np.log10(x_)).reshape((NP, NP))
+p_tot /= p_tot.max()
+
+fig, ax = plt.subplots(figsize=(8, 8))
+#for k in range(1, N_COMPONENTS):
+#    ax.contour(xx, yy, p_components[:, :, k], levels=[0.5], cmap="Reds_r", zorder=-2)
+levels = np.array([0.001, 0.003, 0.01, 0.03, 0.1, 1.0])
+GAMMA = 0.05
+ax.contourf(xx, yy, p_tot**GAMMA, levels=levels**GAMMA, cmap="Greens", zorder=-1, alpha=0.5)
+ax.scatter(
+    10**X[:, 0], 10**X[:, 1], 
+    c=plabels, s=5*np.sqrt(weights), 
+    edgecolors="w", linewidths=1,
+    cmap='seismic')
+ax.set(
+    xscale="log", yscale="log", 
+    xlim=[xmin, xmax], ylim=[ymin, ymax],
+    xlabel=r"Planitude, $\Pi$", ylabel=r"Alatude, $\Lambda$",
+)
+ax.set_aspect("equal");
+
+# So it turns out that we *can* get the components that we could intuitively see, by means of using the weights.
+#
+# However, this is very sensitive to initial conditions.  Re-running the fit can give wildly different decompositions. 
+
+for p in range(N_COMPONENTS):
+    print("COMPONENT", p)
+    print(weights[plabels == p].describe())
+    print()
+
+p_components[:, :, 0].mean()
 
 
